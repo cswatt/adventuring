7be928b93531cee4ebc8b6aca59abaf41f3cf593
Cecilia Watt <ceciliawatt@gmail.com>
2012-10-16 14:50:48 -0400

    try this again

---
+++ app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import cgi
+from xml.dom.minidom import Document
+
+fs = cgi.FieldStorage();
+return str(fs);
---
+++ js/test.js
@@ -0,0 +1,30 @@
+function GetChar(event){
+	var chCode = ('charCode' in event) ? event.charCode : event.keyCode;
+	if (chCode == 13)
+		ProcessInput();
+}
+
+function ProcessInput(){
+	var newinput = $('#input').html(); //get input from box
+	$('#console').append("> " + newinput + '\n'); //add input to textarea
+	$('#console').scrollTop($('#console')[0].scrollHeight); //scroll textarea to bottom
+	$('#input').html(''); //clear input box
+	SendInput(newinput);
+}
+
+function SendInput(contents){
+	$.ajax({
+		url: "/Users/ceciliawatt/Desktop/adventuring/app.py",
+		type: "post",
+		datatype:"text",
+		data: 'hello world',
+		success: function(response){
+			alert(response);
+		}
+	});
+}
+
+/*init*/
+$(document).ready(function(){
+	$('#input').focus();
+});
