abd33fb7c60fb69a4dca3dc7e5291c7abd441c14
Cecilia Watt <ceciliawatt@gmail.com>
2012-10-16 17:50:31 -0400

    messing with app.py for client-side things

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import web
 import os
-from flask import Flask
+import web
+from flask import Flask, Response, send_from_directory, request
 
 def make_text(string):
 	return string
@@ -8,11 +8,15 @@ def make_text(string):
 urls = ('/', 'index')
 render = web.template.render('templates/')
 
-app = web.application(urls, globals())
+app = Flask(__name__)
 
 my_form = web.form.Form(
 	web.form.Textbox('', class_='textfield', id='textfield'),
 	)
+
+@app.route('/')
+def index():
+	return send_from_directory(os.path.join(app.root_path, 'static'), 'index.html')
 
 class tutorial:
 	def GET(self):
