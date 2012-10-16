768f7aaa22155873b9448ede0ee34c8f3f59ea77
Cecilia Watt <ceciliawatt@gmail.com>
2012-10-16 17:43:20 -0400

    for heroku

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-#!/usr/bin/env python
-import cgi
-from xml.dom.minidom import Document
+import web
+import os
+from flask import Flask
 
-fs = cgi.FieldStorage();
-return str(fs);
+def make_text(string):
+	return string
+
+urls = ('/', 'index')
+render = web.template.render('templates/')
+
+app = web.application(urls, globals())
+
+my_form = web.form.Form(
+	web.form.Textbox('', class_='textfield', id='textfield'),
+	)
+
+class tutorial:
+	def GET(self):
+		form = my_form()
+		return render.tutorial(form, "Your text goes here.")
+
+	def POST(self):
+		form = my_form()
+		form.validates()
+		s = form.value['textfield']
+		return make_text(s)
+
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
